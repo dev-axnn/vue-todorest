1897aa073b6e7fde7fc90fc6b2c04c832d07ff60
dev-axnn <dev.axnn@gmail.com>
2022-09-28 13:07:44 +0900

    내용 수정 기능 완료

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h2>Todo View</h2>
     <div v-if="loading">Lading ...</div>
-    <form v-else>
+    <form v-else @submit.prevent="onSave">
       <div class="row">
         <div class="col-6">
           <div class="form-group">
@@ -17,6 +17,8 @@
               <button
                 class="btn"
                 :class="todo.complete ? 'btn-success' : 'btn-danger'"
+                @click="toggleTodoState"
+                type="button"
               >
                 {{ todo.complete ? "Completed" : "Incomplited" }}
               </button>
@@ -25,39 +27,112 @@
         </div>
       </div>
 
-      <button class="btn btn-primary">Save</button>
-      <button class="btn btn-outline-dark ml-2">Cancel</button>
+      <button class="btn btn-primary" type="submit" :disabled="todoState">
+        Save
+      </button>
+      <button class="btn btn-outline-dark ml-2" @click="moveList">
+        Cancel
+      </button>
     </form>
+    <ToastBox v-if="showToast" :message="toastMessage" :color="toastType" />
   </div>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { ref, computed } from "vue";
 import axios from "axios";
+import _ from "lodash";
+import ToastBox from "@/components/ToastBox.vue";
 export default {
+  components: {
+    ToastBox,
+  },
   setup() {
     const route = useRoute();
+    const router = useRouter();
+
     // 데이터로딩 화면창 상태
     const loading = ref(true);
     const todo = ref(null);
+    // 원본 데티어 보관 및 비교용 (todo 객체)
+    const originalTodo = ref(null);
     // 전달받은 id 를 이용해서 db 에서 자료를 가져온다.
     const getTodo = async () => {
       try {
         const response = await axios.get(
           `http://localhost:3000/todos/${route.params.id}`
         );
-        todo.value = response.data;
+        todo.value = { ...response.data };
+        originalTodo.value = { ...response.data };
         loading.value = false;
       } catch (err) {
-        console.log("🚀 ~ file: TodosView.vue ~ line 31 ~ getTodo ~ err", err);
+        triggerToast(
+          "서버에러가 발생하였습니다. 잠시 후에 다시 시도하세요.",
+          "danger"
+        );
       }
     };
     getTodo();
 
+    const toggleTodoState = () => {
+      todo.value.complete = !todo.value.complete;
+    };
+
+    const moveList = () => {
+      router.push({
+        name: "Todos",
+      });
+    };
+
+    const onSave = async () => {
+      try {
+        const res = await axios.put(
+          `http://localhost:3000/todos/${todo.value.id}`,
+          {
+            subject: todo.value.subject,
+            complete: todo.value.complete,
+          }
+        );
+        originalTodo.value = { ...res.data };
+        triggerToast("업데이트 성공!");
+      } catch (err) {
+        triggerToast(
+          "서버에러가 발생하였습니다. 잠시 후에 다시 시도하세요.",
+          "danger"
+        );
+      }
+    };
+
+    const todoState = computed(() => {
+      return _.isEqual(todo.value, originalTodo.value);
+    });
+
+    // 안내창 관련
+    const toastMessage = ref("");
+    const toastType = ref("");
+    const showToast = ref(false);
+    const triggerToast = (massage, color = "success") => {
+      toastMessage.value = massage;
+      toastType.value = color;
+      showToast.value = true;
+      setTimeout(() => {
+        toastMessage.value = "";
+        toastType.value = "";
+        showToast.value = false;
+      }, 3000);
+    };
+
     return {
       todo,
       loading,
+      toggleTodoState,
+      moveList,
+      onSave,
+      todoState,
+      toastMessage,
+      showToast,
+      toastType,
     };
   },
 };
